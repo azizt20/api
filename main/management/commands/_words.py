671fbf1d1a2dfe4af117d97ea1a8c01ce00f3d05
Sniperat <muhammadalixonakbarov2000@gmail.com>
2021-08-11 13:34:30 +0500

    ali

--- main/management/commands/_words.py
+++ main/management/commands/_words.py
@@ -3,40 +3,38 @@ def getword(word, lang):
         '1': {
             'uz': 'Yotoq soni',
             'ru': 'Кол-во спальных мест',
-            'en': 'asd'
+            'en': 'Count of beds'
         },
         '2': {
             'uz': 'Nonushta',
             'ru': 'Завтрак',
-            'en': 'asd'
+            'en': 'Breakfast'
         },
         '3': {
             'uz': 'Yosh bolalar',
             'ru': 'Дети',
-            'en': 'asd'
+            'en': 'Children'
         },
         '4': {
             'uz': 'Uy hayvoni',
             'ru': 'Домашние питомцы',
-            'en': 'asd'
+            'en': 'Pets'
         },
         '5': {
             'uz': 'Band qilish',
             'ru': 'Забронировать',
-            'en': 'asd'
+            'en': 'Book'
         },
         '6': {
-            'uz': 'orqaga',
-            'ru': 'назад ',
-            'en': 'asd'
+            'uz': 'Orqaga',
+            'ru': 'Назад ',
+            'en': 'Back'
         },
         '7': {
             'uz': '''
             Xona narxiga 2 kishilik nonushta kiradi. 3 yoshgacha bo'lgan bolalarga - nonushta bepul. 3 yoshdan ortiq -
           Bir kishiga 100.000 so'm.
-
       Shuningdek, siz mehmonlar ixtiyorida mehmonxona xonasini ijaraga olish majburiyatisiz qo'ng'iroq qilishingiz mumkin
-
       basseyn
       3 ta sauna
       restoran majmuasi
@@ -45,65 +43,76 @@ def getword(word, lang):
             'ru': '''
             ***В стоимость номеров входит завтрак на 2 персоны. Детям до 3х лет - завтрак бесплатный. Свыше 3х лет - 
          100.000 сум за персону.
-
      Также можно заехать без обязательства снятия гостиничного номера, в распоряжении гостей
-
      бассейн
      3 сауны
      ресторанный комплекс
      Стоимость на 1 человека - 300.000 сум, детям до 12 лет бесплатно. Посещение доступно до 18:00.
             ''',
-            'en': 'asd'
+            'en': '''
+	    *** The room rate includes breakfast for 2 persons. Children under 3 years old - breakfast is free. Over 3 years -
+         100,000 soums per person.
+     You can also call in without the obligation to rent a hotel room, at the disposal of guests
+     pool
+     3 saunas
+     restaurant complex
+     The cost for 1 person is 300,000 soums, children up to 12 years old are free. The visit is available until 18:00
+	    '''
         },
         '8': {
             'uz': 'Spa',
             'ru': 'Спа',
-            'en': 'asd'
+            'en': 'Spa'
         },
         '9': {
             'uz': 'Basseyn',
             'ru': 'Бассейн',
-            'en': 'asd'
+            'en': 'Swimming pool'
         },
         '10': {
             'uz': 'Restoran',
             'ru': 'Ресторан',
-            'en': 'asd'
+            'en': 'Restaurant'
         },
         '11': {
             'uz': 'Avvaldan buyurtma bermoq',
             'ru': 'Бронировать',
-            'en': 'asd'
+            'en': 'Book'
         },
         '12': {
             'uz': 'Xonalar',
             'ru': 'Комнаты',
-            'en': 'asd'
+            'en': 'Rooms'
         },
         '13': {
             'uz': 'Shartlar',
             'ru': 'Условия',
-            'en': 'asd'
+            'en': 'Comforts'
         },
         '14': {
             'uz': 'Biz haqimizda',
             'ru': 'О нас',
-            'en': 'asd'
+            'en': 'About us'
         },
         '15': {
             'uz': '''
             HEAVENS GARDEN mehmonxonasi 2020 yil 31 avgustda ochilgan, 46 xonadan iborat;
          12 ta yarimlyuks, 4 ta lyuks, 4 ta ekonom va 26 ta standart. Hovuz tashqarida joylashgan va isitiladi.
          Spa fin saunasi va turk hammomdan iborat. Mehmonxonada bepul internet va sun'iy yo'ldoshli televidenie mavjud.
-       
+
             ''',
             'ru': '''
             Гостиница HEAVENS GARDEN открылась 31 августа 2020 года, имеет 46 номеров;  
         12 полулюксов, 4 люкса, 4 эконома и 26 стандарта. Бассейн расположен на улице и имеет подогрев. 
         Спа состоит из Финской сауны и Турецкого хаммама. В гостинице имеется бесплатный интернет а также спутниковое телевидение.
-        
+
             ''',
-            'en': 'asd'
+            'en': '''
+		HEAVENS GARDEN Hotel opened on August 31, 2020, has 46 rooms;
+        12 junior suites, 4 suites, 4 economy and 26 standards. The pool is located outside and is heated.
+        The spa consists of a Finnish sauna and a Turkish hammam. The hotel has free internet and satellite TV.
+
+	    '''
         },
         '16': {
             'uz': '''
@@ -112,7 +121,9 @@ def getword(word, lang):
             'ru': '''
                 Спа состоит из двух саун(финская,турецкая) и турецкий хаммам
                 ''',
-            'en': 'asd'
+            'en': '''
+		The spa consists of two saunas (Finnish, Turkish) and a Turkish hammam
+		'''
         },
         '17': {
             'uz': '''
@@ -121,85 +132,86 @@ def getword(word, lang):
             'ru': '''
                     Бесплатный открытый бассейн для гостей сделан в формате Endless pool и имеет подогрев.
                     ''',
-            'en': 'asd'
+            'en': '''
+		    Free outdoor pool for guests is made in Endless pool format and is heated.
+	 	    '''
         },
         '18': {
-            'uz': 'bugun',
+            'uz': 'Bugun',
             'ru': 'Cегодня',
-            'en': 'asd'
+            'en': 'Today'
         },
         '19': {
-            'uz': 'ertaga',
-            'ru': 'завтра',
-            'en': 'asd'
+            'uz': 'Ertaga',
+            'ru': 'Завтра',
+            'en': 'Tomorrow'
         },
         '20': {
-            'uz': 'kalendar',
-            'ru': 'календарь',
-            'en': 'asd'
+            'uz': 'Kalendar',
+            'ru': 'Календарь',
+            'en': 'Calendar'
         },
         '21': {
-            'uz': 'kunni tanlash',
-            'ru': 'выберите день',
-            'en': 'asd'
+            'uz': 'Kunni tanlash',
+            'ru': 'Выберите день',
+            'en': 'Choose a day'
         },
         '22': {
             'uz': "Boshlang'ich kunni tanlang",
             'ru': 'Выберите дату начала',
-            'en': 'asd'
+            'en': 'Check-in date'
         },
         '23': {
-            'uz': "oxirgi kunni tanlang ",
-            'ru': 'выберите последний день',
-            'en': 'asd'
+            'uz': "Oxirgi kunni tanlang ",
+            'ru': 'Выберите последний день',
+            'en': 'Check-out day'
         },
         '24': {
             'uz': "Xona olish uchun iltimos shaxsim ma'lumotlaringizni to'ldiring\nTo'liq ismingizni kiriting",
             'ru': 'Чтобы получить номер, введите свои личные данные \nВведите свое полное имя',
-            'en': 'asd'
+            'en': 'To get a number, enter your personal data \nEnter your full name'
         },
         '25': {
             'uz': "Iltmos to'g'ri telefon raqam kiriting",
             'ru': 'Пожалуйста, введите правильный номер телефона',
-            'en': 'asd'
+            'en': 'Please, enter a true phone number'
         },
         '26': {
             'uz': "Iltmos to'g'ri email kiriting",
             'ru': 'Пожалуйста, введите правильный адрес электронной почты',
-            'en': 'asd'
+            'en': 'Please, enter a true mail'
         },
         '27': {
             'uz': "To'liq ismingizni jo'nating",
             'ru': 'Отправьте свое полное имя',
-            'en': 'asd'
+            'en': 'Please, send your fullname	'
         },
         '28': {
             'uz': "Telefon raqamingizni kiriting",
             'ru': 'Введите свой номер телефона',
-            'en': 'asd'
+            'en': 'Please send your number'
         },
         '29': {
             'uz': "Elektron pochtangizni kiriting",
             'ru': 'Введите адрес электронной почты',
-            'en': 'asd'
+            'en': 'Please, send your mail'
         },
         '30': {
             'uz': "Manzilingizni kiriting",
             'ru': 'Введите ваш адрес',
-            'en': 'asd'
+            'en': 'Enter your address'
         },
         '31': {
             'uz': "Asosiy",
-            'ru': 'главный ',
-            'en': 'asd'
+            'ru': 'Главный ',
+            'en': 'Main'
         },
         '32': {
             'uz': "Sizning arizangiz Qabul qilindi javobni kuting",
             'ru': 'Ваша заявка принята. Ждите ответа',
-            'en': 'asd'
+            'en': 'Please, wait, we will contact you shortly'
         }
 
     }
 
     return language[word][lang]
-
